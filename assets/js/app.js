1e47b0d0a46ed3635fca8ad87c2bb5c8f15b8e96
Naman Jyani <namanjyani@Namans-MacBook-Air.local>
2023-11-01 11:36:02 +0530

    nav-bar update

--- assets/js/app.js
+++ assets/js/app.js
@@ -165,3 +165,45 @@ $('.s3-slider').slick({
 
     ]
   });
+  //nav-bar
+  let nav = document.querySelector(".nav");
+let menubar = document.querySelector(".mobile-view");
+let loadtake = document.querySelector(".nav-logo")
+let finish = document.querySelectorAll(".finish");
+let overlay = document.querySelector(".over-layer")
+let bgbody = document.querySelector(".bg-bodys");
+let span1 = document.querySelector(".span1");
+let span2 = document.querySelector(".span2");
+let span3 = document.querySelector(".span3");
+nav.addEventListener("click", function () {
+    menubar.classList.toggle("left-0")
+    nav.classList.toggle("cross-bg")
+    nav.classList.toggle("nav-2")
+    span1.classList.toggle("nav1")
+    span2.classList.toggle("nav2")
+    span3.classList.toggle("nav3")
+    overlay.classList.toggle("left-0")
+    bgbody.classList.toggle("over-flow")
+})
+overlay.addEventListener("click", function () {
+    menubar.classList.toggle("left-0")
+    span1.classList.toggle("nav1")
+    nav.classList.toggle("cross-bg")
+    span2.classList.toggle("nav2")
+    span3.classList.toggle("nav3")
+    overlay.classList.toggle("left-0")
+    bgbody.classList.toggle("over-flow")
+})
+finish.forEach(e => {
+    e.addEventListener("click", function () {
+        menubar.classList.remove("left-0")
+        span1.classList.toggle("nav1")
+        nav.classList.toggle("cross-bg")
+        span2.classList.toggle("nav2")
+        span3.classList.toggle("nav3")
+        bgbody.classList.remove("over-flow")
+        overlay.classList.toggle("left-0")
+        span2.classList.toggle("nav2")
+        span3.classList.toggle("nav3")
+    })
+});
